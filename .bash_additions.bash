7c887829b3453d0439d3139b5ea3c2e2e1164055
Andrew Seward <awswrd@gmail.com>
2013-08-29 13:13:19 -0700

    organizing things...

--- .bash_additions.bash
+++ .bash_additions.bash
@@ -1,6 +1,6 @@
-### asus-small-debian
-if [ -f /etc/bash_completion.d/git ]; then
-    source /etc/bash_completion.d/git
+### universal
+if [ -f ~/.bash_functions.bash ]; then
+    source ~/.bash_functions.bash
 fi
 
 if [ -f ~/.git_functions.bash ]; then
@@ -9,6 +9,11 @@ fi
 
 if [ -f ~/.environment_additions.bash ]; then
     source ~/.environment_additions.bash
+fi
+
+### asus-small-debian
+if [ -f /etc/bash_completion.d/git ]; then
+    source /etc/bash_completion.d/git
 fi
 
 ### pi
@@ -21,10 +26,6 @@ if [ -f /usr/share/git/git-prompt.sh ]; then
 fi
 
 ### ude
-if [ -f ~/.bash_functions.bash ]; then
-    source ~/.bash_functions.bash
-fi
-
 if [ -f ~/.git-completion.bash ]; then
     source ~/.git-completion.bash
 fi
--- .environment_additions.bash
+++ .environment_additions.bash
@@ -1,31 +1,32 @@
+### universal
+export EDITOR='emacs -nw'
+export PATH=$PATH:$HOME/bin
+export capones_dir=$HOME/public_html/cap
+export procore_dir=$HOME/procore
+#export PULSE_SERVER=192.168.1.5
+
+export PS1='[\[\e[35m\]\H\[\e[0m\] \[\e[1;34m\]\w\[\e[0m\] \[$(get_gbranch_colorcode)\]$(gbranch_warn_master)\[\e[0m\]]\$ '
+
 ### asus-small-debian
-
-#PS1='[\[\e[1;32m\]\@\[\e[0m\] \[\e[35m\]\H\[\e[0m\] \[\e[1;34m\]\W\[\e[0m\] \[$(get_gbranch_colorcode)\]$(gbranch_warn_master)\[\e[0m\]]\$ '
-PS1='[\[\e[35m\]\H\[\e[0m\] \[\e[1;34m\]\w\[\e[0m\] \[$(get_gbranch_colorcode)\]$(gbranch_warn_master)\[\e[0m\]]\$ '
-
-PATH=$PATH:~/bin/
-
-export EDITOR='emacs'
-export capones_dir=~/public_html/cap
+if [ "$(hostname)" = "asus-small-debian" ]; then
+    # PS1='[\[\e[1;32m\]\@\[\e[0m\] \[\e[35m\]\H\[\e[0m\] \[\e[1;34m\]\W\[\e[0m\] \[$(get_gbranch_colorcode)\]$(gbranch_warn_master)\[\e[0m\]]\$ '
+    # PS1='[\[\e[35m\]\H\[\e[0m\] \[\e[1;34m\]\w\[\e[0m\] \[$(get_gbranch_colorcode)\]$(gbranch_warn_master)\[\e[0m\]]\$ '
+    cd $capones_dir
+fi
 
 ### pi
-
-#PS1="$(echo $PS1 | sed 's/\\n\\\$//g;s/\\n//g')"
-#PS1=$PS1'\e[36;40m$(__git_ps1 " (%s)")\n\e[m\$ ' # simple but it keeps appending...
-#PS1='['$PS1'\[\e[36m\] \[$(get_gbranch_colorcode)\]$(gbranch)\[\e[36;40m\]\[\e[m\]]\$ '
-#PS1='[\[\e[1;32m\]\@\[\e[0m\] \[\e[35m\]\H\[\e[0m\] \[\e[1;34m\]\W\[\e[0m\] \[$(get_gbranch_colorcode)\]$(gbranch_warn_master)\[\e[0m\]]\$ '
-
-PATH=$PATH:~/bin/
-
-export PULSE_SERVER=192.168.1.5
+if [ "$(hostname)" = "pi" ]; then
+    # PS1="$(echo $PS1 | sed 's/\\n\\\$//g;s/\\n//g')"
+    # PS1=$PS1'\e[36;40m$(__git_ps1 " (%s)")\n\e[m\$ ' # simple but it keeps appending...
+    # PS1='['$PS1'\[\e[36m\] \[$(get_gbranch_colorcode)\]$(gbranch)\[\e[36;40m\]\[\e[m\]]\$ '
+    # PS1='[\[\e[1;32m\]\@\[\e[0m\] \[\e[35m\]\H\[\e[0m\] \[\e[1;34m\]\W\[\e[0m\] \[$(get_gbranch_colorcode)\]$(gbranch_warn_master)\[\e[0m\]]\$ '
+    cd $capones_dir
+fi
 
 ### ude
-
-#PS1='[\[\e[1;32m\]\@\[\e[0m\] \[\e[35m\]\H\[\e[0m\] \[\e[1;34m\]\w\[\e[0m\] \[$(get_gbranch_colorcode)\]$(gbranch_with_warning)\[\e[0m\]]\$ '
-PS1='\[\e[1;35m\]\u\[\e[0m\]@\[\e[32m\]\H\[\e[0m\]:\[\e[1;34m\]\w\[\e[0m\] \[$(get_gbranch_colorcode)\]$(gbranch_with_warning)\[\e[0m\]\$ '
-PATH=$PATH:$HOME/.rvm/bin # can't remember why this is here, but there was a reason
-PATH=$PATH:$HOME/bin
-EDITOR=emacs
-
-# Start in procore dir
-cd /home/andrew/procore
+if [ "$(hostname)" = "developer.andrew" ]; then
+    # PS1='[\[\e[1;32m\]\@\[\e[0m\] \[\e[35m\]\H\[\e[0m\] \[\e[1;34m\]\w\[\e[0m\] \[$(get_gbranch_colorcode)\]$(gbranch_with_warning)\[\e[0m\]]\$ '
+    # PS1='\[\e[1;35m\]\u\[\e[0m\]@\[\e[32m\]\H\[\e[0m\]:\[\e[1;34m\]\w\[\e[0m\] \[$(get_gbranch_colorcode)\]$(gbranch_with_warning)\[\e[0m\]\$ '
+    export PATH=$PATH:$HOME/.rvm/bin # can't remember why this is here, but there was a reason
+    cd $procore_dir
+fi
