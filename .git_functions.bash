4e9e441e65b59662afd95e676ed3ed04c7205478
Andrew Seward <awswrd@gmail.com>
2013-08-29 22:44:11 -0700

    uhhh

--- .git_functions.bash
+++ .git_functions.bash
@@ -66,7 +66,7 @@ git-push () {
         ahead=$(git-commits-ahead)
         if [ $ahead -ne 0 ]; then
             git push origin HEAD
-            echo -e "\nCommits in this push:\n"
+            echo -e "\n\nCommits in this push:\n\n"
             git-last-n-commits $ahead
         else
             echo "No commits to push..."
