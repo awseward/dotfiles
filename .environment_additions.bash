43a0386d23db2e9f9f93adb4b693a9da8dea5a07
Andrew Seward <awswrd@gmail.com>
2013-08-31 00:37:41 -0700

    something funky going on with git-commit

--- .environment_additions.bash
+++ .environment_additions.bash
@@ -4,6 +4,8 @@ export PATH=$PATH:$HOME/bin
 export capones_dir=$HOME/public_html/cap
 export procore_dir=$HOME/procore
 export dotfiles_dir=$HOME/.dotfiles
+export p4d_dir=/cygdrive/c/Users/Andrew/p4d
+export newp4d_dir=/cygdrive/c/Users/Andrew/newp4d
 export init_dir=$HOME # set to home as a fallback
 
 export PS1="[$escaped_bold$escaped_green\u$escaped_clear$escaped_bold@$escaped_clear$escaped_purple\H$escaped_clear $escaped_bold$escaped_blue\w$escaped_clear "'\[$(git-branch-colorcode)\]$(git-branch)'"$escaped_clear]\$ "
@@ -30,6 +32,16 @@ if [ "$(hostname)" = "developer.andrew" ]; then
     fi
 fi
 
+### asus-large
+if [ "$(hostname)" = "asus-large" ]; then
+    if [ -d "$newp4d_dir" ]; then
+        export init_dir=$newp4d_dir
+    elif [-d "$p4d_Dir" ]; then
+        export init_dir=$p4d_dir
+    fi
+fi
+
 if [ -d "$init_dir" ]; then
     cd $init_dir
 fi
+
