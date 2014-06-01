7918fb4078feb335d7e804ca0143a99386debb8b
Andrew Seward <awswrd@gmail.com>
2014-06-01 15:40:05 -0700

    Some find_keywords as post-checkouts

--- git_templates/hooks/find_keywords.sh
+++ git_templates/hooks/find_keywords.sh
@@ -1,7 +1,33 @@
 #!/bin/sh
 
-find_todo ()
+FINDER=
+
+resolve_finder()
 {
-  echo 'TODO: find todos'
+  [ "$FINDER" != "" ] && return
+
+  if [ $(which ag) ]; then
+    FINDER='ag -i --nobreak --noheading'
+  else
+    FINDER='grep -Iris'
+  fi
 }
 
+find_keyword ()
+{
+  [ "$1" = "" ] && return
+  resolve_finder
+  local keyword="$1"
+  local keyword_count=$($FINDER $keyword . | wc -l)
+  echo "$keyword count: $keyword_count"
+}
+
+find_todo ()
+{
+  find_keyword 'TODO'
+}
+ 
+find_console_writeline ()
+{
+  find_keyword 'Console.Writeline'
+}
