f888c35a6143656f68164492fe5a707084377ad8
Andrew Seward <awswrd@gmail.com>
2014-09-09 10:08:51 -0700

    Some tweaks from a while ago

--- tag-cygwin/bash/environment.sh
+++ tag-cygwin/bash/environment.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 
-# pretty sure these are common...
 export EDITOR='vim'
-export WINDOWS_HOME='/cygdrive/c/Users/Andrew'
 export PATH=$PATH:$HOME/bin:/cygdrive/c/Program\ Files/R/R-3.1.0/bin
 export PS1="$escaped_cyan\H$escaped_clear:$escaped_bold$escaped_cyan\W$escaped_clear "'\[$(git_branch_colorcode)\]$(git_branch_brackets)'"$escaped_clear"'$(isGit && echo -n " ")'"$escaped_bold$escaped_green\$$escaped_clear "
 
-init_dir='/cygdrive/c/Users/Andrew/Procore/P4D'
+init_dir="${USERPROFILE}\Procore\ProcoreForDesktop"
 
 alias ghh='cygstart $(origin_url_base)'
 alias ghc='cygstart $(uncolored_git_compare)'
 
-if [ -d "$WINDOWS_HOME" ]; then
-  alias cdw='cd "$WINDOWS_HOME"'
+if [ -d "$USERPROFILE" ]; then
+  alias cdw='cd "$USERPROFILE"'
 fi
 
 if [ -d "$init_dir" ]; then
