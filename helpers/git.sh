9c935ba6bba730510fd0af30605271d0a0f1b10d
Andrew Seward <awswrd@gmail.com>
2014-09-09 22:17:36 -0700

    Some improvements to helpers/git

--- helpers/git.sh
+++ helpers/git.sh
@@ -9,22 +9,38 @@ git_current_branch() {
   git rev-parse --abbrev-ref HEAD | tf -d '\n'
 }
 
+git_remote_host() {
+  __git_parse_remote_host $(__git_filter_remote_v origin)
+}
+
+git_remote_owner() {
+  __git_parse_remote_owner $(__git_filter_remote_v origin)
+}
+
+git_remote_name() {
+  __git_parse_remote_name $(__git_filter_remote_v origin)
+}
+
 git_remote_url() {
-  local remote_string="$(git remote -v | head -n1)"
-  local r_host=$(git_parse_remote_host $remote_string)
-  local r_owner=$(git_parse_remote_owner $remote_string)
-  local r_name=$(git_parse_remote_name $remote_string)
+  local remote_string="$(__git_filter_remote_v origin)"
+  local r_host=$(__git_parse_remote_host $remote_string)
+  local r_owner=$(__git_parse_remote_owner $remote_string)
+  local r_name=$(__git_parse_remote_name $remote_string)
   echo "https://${r_host}/${r_owner}/${r_name}"
 }
 
-git_parse_remote_host() {
+__git_filter_remote_v() {
+  git remote -v | grep -m1 "^${1}\s.*$"
+}
+
+__git_parse_remote_host() {
   echo "$@" | sed -e 's/.\+@\(.\+\.com\).*/\1/'
 }
 
-git_parse_remote_owner() {
+__git_parse_remote_owner() {
   echo "$@" | sed -e 's/.\+\:\(.\+\)\/.*/\1/'
 }
 
-git_parse_remote_name() {
+__git_parse_remote_name() {
   echo "$@" | sed -e 's/.\+\/\(.\+\)\.git.*/\1/'
 }
