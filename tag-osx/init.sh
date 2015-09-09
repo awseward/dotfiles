76f55aa047e27a875d39c6a3cf555df9cd205527
Andrew Seward <awswrd@gmail.com>
2015-09-09 11:04:37 -0700

    No longer need this line

--- tag-osx/init.sh
+++ tag-osx/init.sh
@@ -1,7 +1,6 @@
 # TODO: Figure out os-specific stuff like this someday...
 alias ls="ls -hFG"
 alias rm="rm -dv"
-alias vim="/usr/local/Cellar/vim/7.4.712_1/bin/vim"
 
 # Unset open() from ~/.lib/misc.sh
 unset -f open
