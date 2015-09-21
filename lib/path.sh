dbba38cf931998bc52a8621d3d565c8525c8bc0c
Andrew Seward <awswrd@gmail.com>
2015-09-21 15:40:34 -0700

    Fix some path stuff
    Apparently realpath on OSX doesn't like the quiet flag

--- lib/path.sh
+++ lib/path.sh
@@ -1,12 +1,28 @@
 #!/bin/sh
 
+__canonicalize_path() {
+  realpath "$1"
+}
+
+__prepend_to_PATH() {
+  local entry="$1"
+  [[ "$PATH" =~ (^|:)$entry(:|$) ]] || export PATH="$PATH:$entry"
+}
+
+__append_to_PATH() {
+  local entry="$1"
+  [[ "$PATH" =~ (^|:)$entry(:|$) ]] || export PATH="$entry:$PATH"
+}
+
 pretty_path() {
   echo "$PATH" | tr -s ":" "\n"
 }
 
 warn_if_duplicates_in_path() {
-  local total=$(pretty_path | wc -l)
-  local unique=$(pretty_path | sort | uniq | wc -l)
+  # Using xargs here because wc in osx leaves a bunch of whitespace around and
+  # just throwing it through xargs was the simplest way to trim that
+  local total=$(pretty_path | wc -l | xargs)
+  local unique=$(pretty_path | sort | uniq | wc -l | xargs)
 
   [ "$total" -ne "$unique" ] && echo "Warning: Duplicate entries found in PATH"
 }
