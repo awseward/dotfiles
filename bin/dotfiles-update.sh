2075b5fe09e06c4ab5c85bc50fd5429d35e60daa
Andrew Seward <awswrd@gmail.com>
2015-09-23 16:08:46 -0700

    Fixing some things

--- bin/dotfiles-update.sh
+++ bin/dotfiles-update.sh
@@ -4,17 +4,17 @@
   # Handle args
   _FN_NAME="$1"
   if [ "" = "$_FN_NAME" ]; then
-    _FN_NAME='print_options'
+    _FN_NAME='print_usage'
   fi
 
   # Print info
-  print_options() {
+  print_usage() {
     local function_names
     function_names="$(typeset -f | grep -E '\(\)\ $' | sed -e 's/()//g')"
-    echo "Options:"
-
+    echo "Usage: dotfiles-update.sh [function_name]"
+    echo "  Functions:"
     echo "$function_names" | while read line; do
-      echo "  $line"
+      echo "    $line"
     done
   }
 
@@ -22,29 +22,23 @@
     local last_checked
     local current_time
     local elapsed_time
-    last_checked="$(get_update_timestamp)"
+    last_checked="$(read_timestamp)"
     current_time="$(date +%s)"
     elapsed_time="$((current_time - last_checked))"
 
-    echo "Timestamp file:         $UPDATES_TIMESTAMP_FILEPATH"
-    echo "Update check interval:  ${UPDATE_INTERVAL}s"
-    echo
-    echo "Current time:           $current_time"
-    echo "Last checked:           $last_checked"
-    echo "Elapsed time:           $elapsed_time"
+    echo "Timestamp file        :  $TIMESTAMP_FILEPATH"
+    echo "Update check interval :  ${UPDATE_INTERVAL}s"
+    echo "Current time          :  $current_time"
+    echo "Last checked          :  $last_checked"
+    echo "Elapsed time          :  $elapsed_time"
   }
 
-  print_info_and_options() {
+  print_info_and_usage() {
     print_info
     echo
-    print_options
+    print_usage
   }
 
   # Do something
-  (
-    IFS=''
-    $_FN_NAME | while read line; do
-      echo "  $line"
-    done
-  )
+  $_FN_NAME
 )
--- lib/updates.sh
+++ lib/updates.sh
@@ -1,42 +1,42 @@
 #!/bin/zsh
 
 UPDATE_INTERVAL=$((60 * 60 * 24 * 5))
-UPDATES_TIMESTAMP_FILEPATH="$HOME/.dotfiles-update-timestamp"
+TIMESTAMP_FILEPATH="$HOME/.dotfiles-update-timestamp"
 
-get_update_timestamp() {
-  if dotfiles_update_timestamp_exists; then
-    cat "$UPDATES_TIMESTAMP_FILEPATH"
+read_timestamp() {
+  if $(dotfiles_update_timestamp_exists); then
+    cat "$TIMESTAMP_FILEPATH"
   else
     echo 0
   fi
 }
 
-dotfiles_update_interval_has_passed() {
+update_interval_has_passed() {
   local timestamp
-  timestamp="$(get_update_timestamp)"
+  timestamp="$(read_timestamp)"
 
-  has_update_interval_passed "$timestamp"
+  has_interval_passed "$timestamp"
 }
 
-write_updates_timestamp_file() {
-  date +%s > "$UPDATES_TIMESTAMP_FILEPATH"
+write_timestamp_file() {
+  date +%s > "$TIMESTAMP_FILEPATH"
 }
 
-is_time_to_check_for_updates() {
-  dotfiles_update_interval_has_passed
+is_time_to_check() {
+  update_interval_has_passed
 }
 
-print_dotfiles_update_status() {
-  if $(is_time_to_check_for_updates); then
+status() {
+  if $(is_time_to_check); then
     echo "Time to check updates"
   fi
 }
 
 dotfiles_update_timestamp_exists() {
-  [ -f "$UPDATES_TIMESTAMP_FILEPATH" ]
+  [ -f "$TIMESTAMP_FILEPATH" ]
 }
 
-has_update_interval_passed() {
+has_interval_passed() {
   local timestamp="$1"
   local now
   local actual_interval
