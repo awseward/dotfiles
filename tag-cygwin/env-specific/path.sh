0199f018840eff3e3bffae537c295376d583af44
Andrew Seward <awswrd@gmail.com>
2015-09-25 16:05:56 -0700

    Make cygwin PATH stuff a little prettier

--- tag-cygwin/env-specific/path.sh
+++ tag-cygwin/env-specific/path.sh
@@ -1,18 +1,13 @@
-__ensure_in_PATH /cygdrive/c/Chocolatey/chocolateyinstall
-__ensure_in_PATH /cygdrive/c/ProgramData/chocolatey/chocolateyinstall
 
-__ensure_in_PATH /cygdrive/c/Hashicorp/Vagrant/bin
-
-__ensure_in_PATH '/cygdrive/c/Program Files/nodejs'
-__ensure_in_PATH /cygdrive/c/Users/Andrew/AppData/Roaming/npm
-
-__ensure_in_PATH /cygdrive/c/Windows
-__ensure_in_PATH '/cygdrive/c/Program Files (x86)/Caphyon/Advanced Installer 11.9/bin/x86'
-__ensure_in_PATH '/cygdrive/c/Program Files (x86)/MSBuild/12.0/Bin'
-
-__ensure_in_PATH /cygdrive/c/Go/bin
-
-__ensure_in_PATH '/cygdrive/c/Program Files/Java/jdk1.8.0_40/bin'
-
-__ensure_in_PATH '/cygdrive/c/Program Files (x86)/scala/bin'
-__ensure_in_PATH '/cygdrive/c/Program Files (x86)/sbt/bin'
+__ensure_in_PATH '/cygdrive/c/Chocolatey/chocolateyinstall'                 \
+  '/cygdrive/c/ProgramData/chocolatey/chocolateyinstall'                    \
+  '/cygdrive/c/Hashicorp/Vagrant/bin'                                       \
+  '/cygdrive/c/Program Files/nodejs'                                        \
+  '/cygdrive/c/Users/Andrew/AppData/Roaming/npm'                            \
+  '/cygdrive/c/Windows'                                                     \
+  '/cygdrive/c/Program Files (x86)/Caphyon/Advanced Installer 11.9/bin/x86' \
+  '/cygdrive/c/Program Files (x86)/MSBuild/12.0/Bin'                        \
+  '/cygdrive/c/Go/bin'                                                      \
+  '/cygdrive/c/Program Files/Java/jdk1.8.0_40/bin'                          \
+  '/cygdrive/c/Program Files (x86)/scala/bin'                               \
+  '/cygdrive/c/Program Files (x86)/sbt/bin'                                 \
