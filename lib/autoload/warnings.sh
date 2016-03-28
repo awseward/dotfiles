3b0394a526b9466fae8eb2ac875785e414bd5005
Andrew William Seward <awswrd@gmail.com>
2016-03-28 00:48:06 -0700

    Update some things

--- lib/autoload/warnings.sh
+++ lib/autoload/warnings.sh
@@ -1,5 +1,14 @@
 #!/bin/zsh
 
+_warn() {
+  >&2 echo "Warning: $@"
+}
+
+_error() {
+  >&2 echo "Error: $@"
+  return 1
+}
+
 warn_if_duplicates_in_path() {
   # Using xargs here because wc in osx leaves a bunch of whitespace around and
   # just throwing it through xargs was the simplest way to trim that
@@ -9,14 +18,12 @@ warn_if_duplicates_in_path() {
   unique=$(pretty_path | sort | uniq | wc -l | xargs)
 
   if [ "$total" -ne "$unique" ]; then
-    >&2 echo "Warning: Duplicate entries found in PATH"
-    return 1
+    _warn "Duplicate entries found in PATH"
   fi
 }
 
 warn_if_dotfiles_update_check_recommended() {
   if $(dotfiles-update.sh is_time_to_check); then
-    >&2 echo "Warning: Please check for dotfile updates, then run \`dotfiles-update.sh write_timestamp_file\`."
-    return 1
+    _warn "Please check for dotfile updates, then run \`dotfiles-update.sh write_timestamp_file\`."
   fi
 }
