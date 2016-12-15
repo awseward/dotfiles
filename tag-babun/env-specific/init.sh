58b8829fbfa087734960c155b3fea5fca011ae5a
Drew Seward <awswrd@gmail.com>
2016-12-14 23:55:20 -0800

    Fix broken interactive git on babun
    Upgrading to 2.8.3 screwed some things up with perl...
    
    Fixed it this way:
    https://github.com/babun/babun/issues/557#issuecomment-267261739

--- tag-babun/env-specific/init.sh
+++ tag-babun/env-specific/init.sh
@@ -1,5 +1,4 @@
 # override open from ~/.lib/misc.sh
-
 open() {
   explorer "$1" > /dev/null 2>&1
 }
@@ -10,21 +9,18 @@ print_new_uuid() {
   ruby -e "$rb_cmd"
 }
 
-tmux_socket_workaround() {
-  tmux -L "$(print_new_uuid)"
-}
-
 __set_git_autocrlf() {
   git config --system core.autocrlf true
 }
 
+# I don't really think this actually needs to be a function that is just called
+# right away and then nowhere else...
 __windows_init() {
   __set_git_autocrlf
-  compinit # Not sure why we have to invoke this manually like this now...
+  compinit # Not sure why this needs to be invoked manually like this now...
 
   alias cdw='cd "$USERPROFILE"'
+  export PERL5LIB=/usr/lib/perl5/vendor_perl/5.14
 }
 
 __windows_init
-
-alias tmx='tmux_socket_workaround'
