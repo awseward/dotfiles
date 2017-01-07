d52721f535c5661a008b65ac6a1e5a017bcfd40e
Drew Seward <awswrd@gmail.com>
2017-01-06 17:43:56 -0800

    Add a couple Windows niceties

--- tag-babun/env-specific/init.sh
+++ tag-babun/env-specific/init.sh
@@ -9,18 +9,13 @@ print_new_uuid() {
   ruby -e "$rb_cmd"
 }
 
-__set_git_autocrlf() {
-  git config --system core.autocrlf true
-}
+# Env vars
+export PERL5LIB=/usr/lib/perl5/vendor_perl/5.14
 
-# I don't really think this actually needs to be a function that is just called
-# right away and then nowhere else...
-__windows_init() {
-  __set_git_autocrlf
-  compinit # Not sure why this needs to be invoked manually like this now...
+# Aliases
+alias cdw='cd "$USERPROFILE"'
+alias psh='cmd /c start powershell'
 
-  alias cdw='cd "$USERPROFILE"'
-  export PERL5LIB=/usr/lib/perl5/vendor_perl/5.14
-}
-
-__windows_init
+# Misc
+git config --system core.autocrlf true
+compinit # Not sure why this needs to be invoked manually like this now...
