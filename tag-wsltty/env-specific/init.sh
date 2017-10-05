9278effd354ad5e47cf278c1b2cacf9a09b8adc9
Drew Seward <awswrd@gmail.com>
2017-10-04 21:03:34 -0700

    Add win-env
    Read arbitrary windows environment variables

--- tag-wsltty/env-specific/init.sh
+++ tag-wsltty/env-specific/init.sh
@@ -1,24 +1,3 @@
-# TODO: Parameterize for arbitary windows env vars
-__get_USERPROFILE() {
-  local userprofile_win_path
-  userprofile_win_path="$(cmd.exe /C 'echo %USERPROFILE%' 2>&1 | tail -n 1 | sed 's/\r//g')"
-
-  echo "$(wslpath -u $userprofile_win_path)"
-}
-
-export LS_COLORS="ow=0"
-export SHELL="/bin/zsh" # isn't getting updated/set propertly by chsh, so just setting it manually here
-export USERPROFILE="$(__get_USERPROFILE)"
-
-alias cdw='cd "$USERPROFILE"'
-alias fake="./fake.sh "
-alias hub="hub.exe " # should be temporary... find a better way
-alias open="explorer.exe" # override open from ~/.lib/misc.sh
-alias paket="./paket.sh "
-
-umask 022 # WSL Windows /mnt/* dir permissions workaround
-git config --system core.autocrlf true
-
 psh() {
   local psh_path
 
@@ -30,3 +9,40 @@ psh() {
 
   cmd.exe /c start powershell -noexit -command "cd $(wslpath -w $psh_path)"
 }
+
+win_env() {
+  if [ "$1" = "" ]; then
+    return 1
+  fi
+
+  local paren_wrapped
+  paren_wrapped="%$1%"
+
+  local cmd_exe_arg
+  cmd_exe_arg="echo $paren_wrapped"
+
+  local cmd_result
+  cmd_result="$(cmd.exe /C $cmd_exe_arg 2>&1 | tail -n1 | sed 's/\r$//g')"
+
+  if [ "$cmd_result" = "$paren_wrapped" ]; then
+    echo
+  else
+    echo -E "$cmd_result"
+  fi
+}
+
+export LS_COLORS="ow=0"
+export SHELL="/bin/zsh" # isn't getting updated/set propertly by chsh, so just setting it manually here
+
+export USERPROFILE="$(wslpath -u $(win_env 'USERPROFILE'))"
+export LOCALAPPDATA="$(wslpath -u $(win_env 'LOCALAPPDATA'))"
+
+alias fake="./fake.sh "
+alias hub="hub.exe " # should be temporary... find a better way
+alias lappd='cd "$LOCALAPPDATA"'
+alias open="explorer.exe" # override open from ~/.lib/misc.sh
+alias paket="./paket.sh "
+alias uspro='cd "$USERPROFILE"'
+
+umask 022 # WSL Windows /mnt/* dir permissions workaround
+git config --system core.autocrlf true
