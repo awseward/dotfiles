6636bc1b89346c82cb3600d16eaa3dd70c8df3a5
Andrew Seward <awswrd@gmail.com>
2018-09-09 17:22:57 -0700

    fix: Move iterm integration to OSX init

--- tag-osx/env-specific/init.sh
+++ tag-osx/env-specific/init.sh
@@ -3,3 +3,5 @@ export JAVA_HOME="$(/usr/libexec/java_home)"
 export RACKET_BIN_DIR="/Applications/Racket v7.0/bin/"
 
 __ensure_in_PATH "/usr/local/sbin"
+
+test -e "${HOME}/.iterm2_shell_integration.zsh" && source "${HOME}/.iterm2_shell_integration.zsh"
