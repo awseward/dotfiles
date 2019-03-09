abb41be925d030bce4f60625aa890e81d6b9cf46
Andrew Seward <awswrd@gmail.com>
2019-03-09 01:58:34 -0800

    Become more Brewfile-based

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-## Dotfiles
+# dotfiles
+
+## MacOS
+
+
+```sh
+git clone git@github.com:awseward/dotfiles.git ~/.dotfiles
+~/.dotfiles/setup/macos.sh
+```
---
+++ setup/macos.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+dotfiles="$(dirname "$0")/.."
+
+which brew >/dev/null 2>&1 \
+  || /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" \
+  || echo 'Something went wrong installing Homebrew... Try https://brew.sh.'
+
+set -o xtrace
+
+brew bundle --file="$dotfiles"/Brewfile
+
+rcup -v -t osx
+
+# This is a bit wacky
+spectacle_shortcuts="Library/Application Support/Spectacle/Shortcuts.json"
+spec_src="$dotfiles/$spectacle_shortcuts"
+spec_dst="$HOME/$spectacle_shortcuts"
+
+diff "$spec_src" "$spec_dst" >/dev/null 2>&1 \
+  || cp -i "$dotfiles/$spectacle_shortcuts" "$HOME/$spectacle_shortcuts"
