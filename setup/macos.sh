6185c65a7e2839d7b7e502409022fe15b38d3beb
Andrew Seward <awswrd@gmail.com>
2019-03-12 23:22:50 -0700

    misc: Remove the xtrace

--- setup/macos.sh
+++ setup/macos.sh
@@ -8,16 +8,14 @@ which brew >/dev/null 2>&1 \
   || /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" \
   || echo 'Something went wrong installing Homebrew... Try https://brew.sh.'
 
-set -o xtrace
 
 brew bundle --file="$dotfiles"/Brewfile
 
 rcup -v -t osx
 
-# This is a bit wacky
+# This is a bit wacky, but couldn't quite get rcm to do what I wanted here
 spectacle_shortcuts="Library/Application Support/Spectacle/Shortcuts.json"
 spec_src="$dotfiles/tag-osx/$spectacle_shortcuts"
 spec_dst="$HOME/$spectacle_shortcuts"
-
 diff "$spec_src" "$spec_dst" >/dev/null 2>&1 \
-  || cp -i "$dotfiles/$spectacle_shortcuts" "$HOME/$spectacle_shortcuts"
+  || cp -iv "$spec_src" "$spec_dst"
