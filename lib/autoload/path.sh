dea473b39afecba213c8108c0697a0a6908d3b06
Andrew Seward <awswrd@gmail.com>
2019-03-13 00:51:32 -0700

    misc: Set up env-specific post-up

--- lib/autoload/path.sh
+++ lib/autoload/path.sh
@@ -1,4 +1,4 @@
-#!/bin/zsh
+#!/usr/bin/env zsh
 
 __canonicalize_path() {
   case "$OSTYPE" in
--- setup/macos.sh
+++ setup/macos.sh
@@ -2,20 +2,13 @@
 
 set -euo pipefail
 
-dotfiles="$(dirname "$0")/.."
+dotfiles="$HOME/.dotfiles"
 
 which brew >/dev/null 2>&1 \
   || /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" \
   || echo 'Something went wrong installing Homebrew... Try https://brew.sh.'
 
 
-brew bundle --file="$dotfiles"/Brewfile
+brew bundle --file="$HOME/.dotfiles/Brewfile"
 
 rcup -v -t osx
-
-# This is a bit wacky, but couldn't quite get rcm to do what I wanted here
-spectacle_shortcuts="Library/Application Support/Spectacle/Shortcuts.json"
-spec_src="$dotfiles/tag-osx/$spectacle_shortcuts"
-spec_dst="$HOME/$spectacle_shortcuts"
-diff "$spec_src" "$spec_dst" >/dev/null 2>&1 \
-  || cp -iv "$spec_src" "$spec_dst"
