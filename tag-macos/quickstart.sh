c79020dc3673e582f2af89b942f78203ccc1498b
Andrew Seward <awswrd@gmail.com>
2019-03-30 20:10:31 -0700

    Rework quickstart to not pipe curl into shell
    At least puts it on disk before running it.
    
    Not sure if it would actually even be a problem if the script died
    halfway through the other way, but /shrug.
    
    Maybe next is to print the file out into a pager and add a prompt or
    something so that person executing the quickstart can audit and
    confirm/deny.

--- tag-macos/quickstart.sh
+++ tag-macos/quickstart.sh
@@ -5,10 +5,46 @@ set -euo pipefail
 dotfiles="$HOME/.dotfiles"
 tag="macos"
 
-which brew >/dev/null 2>&1 \
-  || /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" \
-  || echo 'Something went wrong installing Homebrew... Try https://brew.sh.'
+function _ensure_brew_installed() {
+  if ! type brew; then
+    local install_file
+    install_file="$(mktemp)"
+    curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install > "install_file"
+    /usr/bin/ruby "$install_file"
 
-brew bundle --file="$dotfiles/tag-$tag/Brewfile"
+    [ $? != 0 ] \
+      && >&2 echo 'Failure installing Homebrew... See: https://brew.sh' \
+      && exit 1
+
+    rm -f "$install_file"
+  fi
+}
+
+function _install_brew_deps() {
+  brew bundle --file="$dotfiles/tag-$tag/Brewfile"
+}
+
+function _ensure_omz_installed() {
+  if [ ! -d "$HOME/.oh-my-zsh" ]; then
+    local install_file
+    install_file="$(mktemp)"
+    # NOTE: the pipe to sed here is to prevent install.sh from starting a new
+    #       login shell, which prevents anything after this from being run.
+    curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh \
+      | sed -e 's/env\ zsh\ \-l//g' \
+      > "$install_file"
+    sh "$install_file"
+
+    [ $? != 0 ] \
+      && >&2 echo 'Failure installing Oh My Zsh... See: https://ohmyz.sh/' \
+      && exit 1
+
+    rm -f "$install_file"
+  fi
+}
+
+_ensure_brew_installed
+_install_brew_deps
+_ensure_omz_installed
 
 export RCRC="$dotfiles/rcrc" && rcup -v -d "$dotfiles" -t "$tag"
