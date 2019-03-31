98bb8a483d74d58bfce83ef8a86fea1a5509da35
Andrew Seward <awswrd@gmail.com>
2019-03-30 21:04:34 -0700

    Add review/prompt to install for brew, omz

--- tag-macos/quickstart.sh
+++ tag-macos/quickstart.sh
@@ -5,19 +5,33 @@ set -euo pipefail
 dotfiles="$HOME/.dotfiles"
 tag="macos"
 
+function _review() {
+  local install_file
+  local answer
+  install_file="$1"
+  cat "$install_file" && echo -n "Execute [yN]? " && read answer
+
+  [ "$answer" != "${answer#[Yy]}" ] \
+    && >&2 echo "Aborting quickstart (chose not to execute $install_file)" \
+    && exit 1
+}
+
 function _ensure_brew_installed() {
-  if ! type brew; then
-    local install_file
-    install_file="$(mktemp)"
-    curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install > "install_file"
-    /usr/bin/ruby "$install_file"
+  if type brew; then return 0; fi # skip if brew installed
 
-    [ $? != 0 ] \
-      && >&2 echo 'Failure installing Homebrew... See: https://brew.sh' \
-      && exit 1
+  local install_file
+  install_file="$(mktemp)"
+  curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install > "install_file"
 
-    rm -f "$install_file"
-  fi
+  _review "$install_file"
+
+  /usr/bin/ruby "$install_file"
+
+  [ $? != 0 ] \
+    && >&2 echo 'Failure installing Homebrew... See: https://brew.sh' \
+    && exit 1
+
+  rm "$install_file"
 }
 
 function _install_brew_deps() {
@@ -25,26 +39,33 @@ function _install_brew_deps() {
 }
 
 function _ensure_omz_installed() {
-  if [ ! -d "$HOME/.oh-my-zsh" ]; then
-    local install_file
-    install_file="$(mktemp)"
-    # NOTE: the pipe to sed here is to prevent install.sh from starting a new
-    #       login shell, which prevents anything after this from being run.
-    curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh \
-      | sed -e 's/env\ zsh\ \-l//g' \
-      > "$install_file"
-    sh "$install_file"
+  if [ -d "$HOME/.oh-my-zsh" ]; then return 0; fi # skip if omz installed
 
-    [ $? != 0 ] \
-      && >&2 echo 'Failure installing Oh My Zsh... See: https://ohmyz.sh/' \
-      && exit 1
+  local install_file
+  install_file="$(mktemp)"
+  # NOTE: the pipe to sed here is to prevent install.sh from starting a new
+  #       login shell on us, which prevents anything after this from being run.
+  curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh \
+    | sed -e 's/env\ zsh\ \-l//g' \
+    > "$install_file"
 
-    rm -f "$install_file"
-  fi
+  _review "$install_file"
+
+  sh "$install_file"
+
+  [ $? != 0 ] \
+    && >&2 echo 'Failure installing Oh My Zsh... See: https://ohmyz.sh/' \
+    && exit 1
+
+  rm "$install_file"
 }
 
-_ensure_brew_installed
-_install_brew_deps
-_ensure_omz_installed
+function main {
+  _ensure_brew_installed
+  _install_brew_deps
+  _ensure_omz_installed
 
-export RCRC="$dotfiles/rcrc" && rcup -v -d "$dotfiles" -t "$tag"
+  export RCRC="$dotfiles/rcrc" && rcup -v -d "$dotfiles" -t "$tag"
+}
+
+main
