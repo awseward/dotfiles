6b3f554cafe7c5abdc5ca37873665d23814d7b2f
Andrew Seward <awswrd@gmail.com>
2019-03-30 21:40:28 -0700

    Misc quickstart tweaks

--- quickstart.sh
+++ quickstart.sh
@@ -2,17 +2,30 @@
 
 set -euo pipefail
 
-case "$OSTYPE" in
-  (darwin*)
-    export RCM_TAG=macos
-    export DOTFILES="$HOME/.dotfiles"
+export DOTFILES="$HOME/.dotfiles"
 
-    git clone git@github.com:awseward/dotfiles.git "$DOTFILES"
-    "$DOTFILES"/tag-$RCM_TAG/quickstart.sh
-  ;;
-  (*)
-    >&2 echo 'Quickstart script currently only set up for OSX. Sorry =/'
+function _resolve_rcm_tag() {
+  case "$OSTYPE" in
+    (darwin*)
+      export RCM_TAG=macos
+    ;;
+  # More if necessary...
+  esac
+
+  if [ -z ${RCM_TAG+x} ]; then
+    >&2 cat <<ERR
+
+Bummer.
+
+It appears this quickstart script couldn't resolve your OS to an RCM tag.
+
+For more more info on RCM, see: https://github.com/thoughtbot/rcm#readme
+
+ERR
     exit 1
-  ;;
-esac
+  fi
+}
 
+
+git clone git@github.com:awseward/dotfiles.git "$DOTFILES"
+"$DOTFILES"/tag-$RCM_TAG/quickstart.sh
