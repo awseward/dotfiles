18ae9dc0770ac8ac267eaa54747d2ef48c7c7ec7
Andrew Seward <awswrd@gmail.com>
2019-07-05 12:07:22 -0700

    Convert 'jr', 'pv' aliases to keybindings

--- lib/autoload/misc.sh
+++ lib/autoload/misc.sh
@@ -24,3 +24,24 @@ list_functions() {
   esac
 }
 
+if type "fzf" > /dev/null; then
+  # (jr for 'Jump to Repo')
+  #
+  # NOTE: This assumes a convention of repositories being located in:
+  #       `$HOME/projects/$LANG_OR_CONTEXT/`
+  function _jump-repo {
+    local repo="$(find -H "$HOME/projects" -mindepth 2 -maxdepth 2 -type d -or -type l | fzf --height 25% --reverse --border --header='Repositories:')"
+    if [ "$repo" != '' ]; then
+      cd "$repo" && pwd && l
+    fi
+    zle reset-prompt
+  }
+  zle -N _jump-repo && bindkey '^j^r' _jump-repo
+
+  # (pv for 'PreView')
+  function _preview-wd {
+    ls | fzf --border --preview 'bat --style=numbers --color=always {}' --preview-window=right:60%:wrap --bind 'enter:accept+execute(vim {})'
+    zle reset-prompt
+  }
+  zle -N _preview-wd && bindkey '^p^v' _preview-wd
+fi
