313a95188aa70ffd0092733fcd65fc58d92e716f
Andrew Seward <awswrd@gmail.com>
2019-07-23 07:26:01 -0700

    wip: Maybe it's the leading caret
    It does seem to be hanging here at this grep step...

--- actions-shellmisc/entrypoint.sh
+++ actions-shellmisc/entrypoint.sh
@@ -17,14 +17,6 @@ _detect_missing_shebang() {
   echo "$f_name"
 }
 
-_detect_zsh_shebang() {
-  local f_name="$1"
-
-  if head -n1 "$f_name" | command grep -q -E '^#![^ ]+\ zsh'; then
-    echo "$f_name"
-  fi
-}
-
 _warn_if_missing_shebangs() {
   local _sh_or_bash_files
   local _missing_shebangs
@@ -45,7 +37,7 @@ WRN
 
 _warn_if_zsh_shebangs() {
   local _zsh_shebang_files
-  _zsh_shebang_files="$(grep -rlE '^#!/.*\ zsh' .)"
+  _zsh_shebang_files="$(grep -rlE '#!/.*\ zsh' .)"
 
   if [ "$_zsh_shebang_files" != "" ]; then
     _warn_stdin <<WRN
