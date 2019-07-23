7c470e4799bfbd2445627e04df216a4f51476b14
Andrew Seward <awswrd@gmail.com>
2019-07-23 07:01:16 -0700

    misc: Remove some curly braces where unnecessary

--- tag-macos/env-specific/init.sh
+++ tag-macos/env-specific/init.sh
@@ -11,12 +11,12 @@ __ensure_in_PATH "/usr/local/sbin"
 __ensure_in_PATH '/Applications/SnowSQL.app/Contents/MacOS'
 
 # shellcheck source=/dev/null
-test -e "${HOME}/.iterm2_shell_integration.zsh" && source "${HOME}/.iterm2_shell_integration.zsh"
+test -e "$HOME/.iterm2_shell_integration.zsh" && source "$HOME/.iterm2_shell_integration.zsh"
 
 # https://gist.github.com/bmhatfield/f613c10e360b4f27033761bbee4404fd
 # Usage: keychain-environment-variable SECRET_ENV_VAR
 function keychain-environment-variable () {
-    security find-generic-password -w -a "${USER}" -D "environment variable" -s "${1}"
+    security find-generic-password -w -a "$USER" -D "environment variable" -s "$1"
 }
 
 HOMEBREW_GITHUB_API_TOKEN="$(keychain-environment-variable HOMEBREW_GITHUB_API_TOKEN)"
