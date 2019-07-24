925c504021b4136e1091d0d2e043e9cb21a79e1c
Andrew Seward <awswrd@gmail.com>
2019-07-24 15:35:22 -0700

    fix: Just avoid recursive grep altogether

--- actions-shellmisc/entrypoint.sh
+++ actions-shellmisc/entrypoint.sh
@@ -2,8 +2,12 @@
 
 set -eu
 
-_find_files_by_extension() {
+_find_shell_files_by_ext() {
   find . -type f -name '*.sh' -or -name '*.bash'
+}
+
+_find_files_excluding_dot_dirs() {
+  find . -type f -not -path './.*'
 }
 
 _warn_stdin() {
@@ -23,11 +27,20 @@ _detect_missing_shebang() {
   fi
 }
 
+_detect_zsh_shebang() {
+  local f_name="$1"
+  if head -n1 "$f_name" | command grep -q -E '^#!/.*\ zsh'; then
+    return 0
+  else
+    echo "$f_name"
+  fi
+}
+
 _warn_if_missing_shebangs() {
   echo "Checking for missing shebangs..."
 
   local _missing_shebangs
-  _missing_shebangs="$(_find_files_by_extension | while read -r line; do _detect_missing_shebang "$line"; done)"
+  _missing_shebangs="$(_find_shell_files_by_ext | while read -r line; do _detect_missing_shebang "$line"; done)"
 
   if [ "$_missing_shebangs" != "" ]; then
     _warn_stdin <<WRN
@@ -47,10 +60,9 @@ _warn_if_zsh_shebangs() {
   echo "Checking for unwanted zsh shebangs..."
 
   find . -type f -not -path './.*'
-  return 0
 
   local _zsh_shebang_files
-  _zsh_shebang_files="$(find . -type f -not -path './.*' | grep -rlE '^#!/.*\ zsh' .)"
+  _zsh_shebang_files="$(_find_files_excluding_dot_dirs | while read -r line; do _detect_zsh_shebang "$line"; done)"
 
   if [ "$_zsh_shebang_files" != "" ]; then
     _warn_stdin <<WRN
