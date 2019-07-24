c1d5a2dea00af91c7dc2f2045560e89c81308a2e
Andrew Seward <awswrd@gmail.com>
2019-07-24 15:25:02 -0700

    misc: Add a success message

--- actions-shellmisc/entrypoint.sh
+++ actions-shellmisc/entrypoint.sh
@@ -10,6 +10,10 @@ _warn_stdin() {
   2>&1 echo "WARNING: $(cat -)"
 }
 
+_ok() {
+  echo "👍 $*"
+}
+
 _detect_missing_shebang() {
   local f_name="$1"
   if head -n1 "$f_name" | command grep -q -E '^#!'; then
@@ -32,6 +36,8 @@ $_missing_shebangs
 Please specify a shebang in these files.
 
 WRN
+  else
+    _ok "No missing shebangs detected!"
   fi
 }
 
@@ -48,6 +54,8 @@ $_zsh_shebang_files
 Please try to refrain from declaring dependency on zsh if possible. Try preferring bash.
 
 WRN
+  else
+    _ok "No zsh shebangs found!"
   fi
 }
 
