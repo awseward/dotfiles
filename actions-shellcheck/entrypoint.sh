457d99700fc9c89d0acaefe3a1681ca0a0ad6195
Andrew Seward <awswrd@gmail.com>
2019-07-24 20:26:46 -0700

    misc: More changes

--- actions-shellcheck/entrypoint.sh
+++ actions-shellcheck/entrypoint.sh
@@ -2,9 +2,10 @@
 
 set -eu
 
-_files="$(grep -rlE '#!/.*\ (ba)?sh' .)"
-
 function _run_warning() {
+  echo "Checking for errors or warnings..."
+  local _files="$1"
+
   if echo "$_files" | xargs -t shellcheck --wiki-link-count=1000 --severity=warning; then
     echo "👍 No warnings or errors from shellcheck!"
     echo
@@ -13,12 +14,20 @@ function _run_warning() {
   fi
 }
 
-function _run_all() {
-  echo "$_files" | xargs -t shellcheck --wiki-link-count=1000 || return 0
+function _run_any() {
+  echo "Checking for any actionable issues..."
+  local _files="$1"
+
+  if echo "$_files" | xargs -t shellcheck --wiki-link-count=1000; then
+    echo "👍 No actionable issues from shellcheck!"
+    echo
+  fi
 }
 
 echo
 shellcheck --version
 echo
 
-_run_warning && _run_all
+_files="$(grep -rlE '#!/.*\ (ba)?sh' .)"
+
+_run_warning "$_files" && _run_any "$_files"
