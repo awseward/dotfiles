5e8647011ca0ded7029dc0cc422bdc8af25f4fbd
Andrew Seward <awswrd@gmail.com>
2019-07-24 20:10:43 -0700

    fix: Add some separation around shellcheck info

--- actions-shellcheck/entrypoint.sh
+++ actions-shellcheck/entrypoint.sh
@@ -7,6 +7,7 @@ _files="$(grep -rlE '#!/.*\ (ba)?sh' .)"
 function _run_warning() {
   if echo "$_files" | xargs -t shellcheck --wiki-link-count=1000 --severity=warning; then
     echo "👍 No warnings or errors from shellcheck!"
+    echo
   else
     return 1
   fi
@@ -16,6 +17,8 @@ function _run_all() {
   echo "$_files" | xargs -t shellcheck --wiki-link-count=1000 || return 0
 }
 
+echo
 shellcheck --version
+echo
 
 _run_warning && _run_all
