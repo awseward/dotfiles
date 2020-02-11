1763c2c1e4f35c10edeb6ea9a2cba660d2a32de3
Andrew Seward <awswrd@gmail.com>
2020-02-10 18:18:28 -0800

    wip: Some refinement

--- lib/functions/git_files_view.sh
+++ lib/functions/git_files_view.sh
@@ -6,28 +6,46 @@ ghv() {
   local temp_dir; temp_dir="$(mktemp -d)"
   local git_sha="${1:-$(git_current_branch)}"
 
-  do_the_thing() {
-    git files "origin/master...${git_sha}" | while read -r filename; do
-      local temp_filename="${temp_dir}/${filename}"
-      dirname "$temp_filename" | xargs -t mkdir -p -
-      git show "${git_sha}:${filename}" > "$temp_filename"
-    done
-    local v_lines; v_lines="$(tput lines)"
+  cleanup() { rm -rf "${temp_dir}" >/dev/null 2>&1; }
+  changed_files() { git diff --name-only "origin/master...${git_sha}"; }
+  populate_local_disk() {
+    changed_files | while read -r filename; do
+      local ref_filename="${git_sha}:${filename}"
 
-    # TODO: Make this a little smarter
-    ( \
+      git cat-file -e "${ref_filename}" 2>/dev/null && {
+        local temp_filename="${temp_dir}/${filename}"
+
+        dirname "$temp_filename" | xargs mkdir -p -
+
+        git show "${ref_filename}" > "$temp_filename"
+      }
+    done
+  }
+  fzf_prev() {
+    local preview_window
+
+    {
+      if (( $(tput cols) > 120 )); then
+        preview_window="left:$(( $(tput cols) - 80 ))"
+      else
+        preview_window="up:$(( $(tput lines) - 10 ))"
+      fi
+    }
+
+    (
       cd "${temp_dir}" \
         && fzf \
           --preview 'bat --style=numbers --color=always {}' \
-          --preview-window "up:$((v_lines - 10))" \
+          --preview-window "${preview_window}" \
           --bind 'enter:execute(vim -R -c "set nomodifiable" {})'
     )
-
-    # Nice for jumping from one file to another
-    # vim -R -c 'set nomodifiable' -c "cd ${temp_dir}"
   }
 
-  cleanup() { rm -rf "${temp_dir}" >/dev/null 2>&1 ; }
+  # ---
 
-  do_the_thing || cleanup && cleanup
+  if [ $(changed_files | wc -l) = 0 ]; then
+    return 1
+  fi
+
+  { populate_local_disk || true && fzf_prev } || true && cleanup
 }
