00018db19ff9f87682cb8311d02bdf7c9bb76169
Andrew Seward <awswrd@gmail.com>
2020-10-26 16:28:14 -0700

    Try nix out a little bit
    See: https://web.archive.org/web/20201026013420/https://wickedchicken.github.io/post/macos-nix-setup/
    
    ¯\_(ツ)_/¯
    
    TODO: Should probably comment about the python vim thing, done to solve
    a YouCompleteMe error

---
+++ nixpkgs/darwin-configuration.nix
@@ -0,0 +1,20 @@
+{ config, pkgs, ... }:
+
+{
+  # List packages installed in system profile. To search by name, run:
+  # $ nix-env -qaP | grep wget
+  environment.systemPackages = with pkgs; [
+    (vim_configurable.override { python = python3; })
+  ];
+
+  # Auto upgrade nix package and the daemon service.
+  services.nix-daemon.enable = true;
+  nix.package = pkgs.nix;
+
+  # Create /etc/zshrc that loads the nix-darwin environment.
+  programs.zsh.enable = true;  # default shell on catalina
+
+  # Used for backwards compatibility, please read the changelog before changing.
+  # $ darwin-rebuild changelog
+  system.stateVersion = 4;
+}
