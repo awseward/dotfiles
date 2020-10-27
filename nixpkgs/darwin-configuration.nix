a687b09f53a76d6a890e10c9121d81a4243361b1
Andrew Seward <awswrd@gmail.com>
2020-10-26 21:24:45 -0700

    Move shellcheck into nix

--- nixpkgs/darwin-configuration.nix
+++ nixpkgs/darwin-configuration.nix
@@ -5,6 +5,7 @@
   # $ nix-env -qaP | grep wget
   environment.systemPackages = with pkgs; [
     config.programs.vim.package
+    shellcheck
   ];
 
   # Auto upgrade nix package and the daemon service.
