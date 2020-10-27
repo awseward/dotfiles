c00d42acee11bba0a6b1c224aee6dc948cf270f9
Andrew Seward <awswrd@gmail.com>
2020-10-27 12:52:32 -0700

    Move redis into nix also
    Thought it'd be a good opportunity to not have to do the whole
    `stop-writes-on-bgsave-error no` dance every single time it updates, but
    not quite; it seems like the nix-darwin package will need a small
    addition before that's possible.
    
    Also I had to do this to get it to start: `sudo mkdir /var/lib/redis`

--- nixpkgs/darwin-configuration.nix
+++ nixpkgs/darwin-configuration.nix
@@ -21,6 +21,7 @@
     figlet
     htop
     jq
+    redis
     ripgrep
     shellcheck
     sox
@@ -33,14 +34,25 @@
   services.nix-daemon.enable = true;
   nix.package = pkgs.nix;
 
-  # Create /etc/zshrc that loads the nix-darwin environment.
-  programs.zsh.enable = true;  # default shell on catalina
+  services.redis = {
+    enable = true;
+    # FIXME: At the time of writing, the nix-darwin module for this does not expose
+    # `extraConfig` the same way the nixpkgs one does...
+    # See: https://github.com/LnL7/nix-darwin/blob/master/modules/services/redis/default.nix)
+    #
+    # extraConfig = ''
+    #   stop-writes-on-bgsave-error no
+    # '';
+  };
 
   programs.vim.package = pkgs.vim_configurable.override {
     darwinSupport = true;
     guiSupport = "no";
     python = pkgs.python3;
   };
+
+  # Create /etc/zshrc that loads the nix-darwin environment.
+  programs.zsh.enable = true;  # default shell on catalina
 
   # Used for backwards compatibility, please read the changelog before changing.
   # $ darwin-rebuild changelog
