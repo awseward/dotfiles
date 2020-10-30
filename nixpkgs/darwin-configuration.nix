bb10141be3ac6c69b059a76b3b4dfeda255e4ad2
Andrew Seward <awswrd@gmail.com>
2020-10-30 01:09:34 -0700

    Steal some macOS niceties from lnl.nix
    https://github.com/LnL7/nix-darwin/blob/c2124383f47315c23b0fbe8b75a1190b6d7929d0/modules/examples/lnl.nix#L8-L34

--- nixpkgs/darwin-configuration.nix
+++ nixpkgs/darwin-configuration.nix
@@ -1,6 +1,27 @@
 { config, pkgs, ... }:
 
 {
+
+  system.defaults.NSGlobalDomain.ApplePressAndHoldEnabled = false;
+  system.defaults.NSGlobalDomain.InitialKeyRepeat = 1;
+  system.defaults.NSGlobalDomain.KeyRepeat = 1;
+  system.defaults.NSGlobalDomain.NSAutomaticCapitalizationEnabled = false;
+  system.defaults.NSGlobalDomain.NSAutomaticDashSubstitutionEnabled = false;
+  system.defaults.NSGlobalDomain.NSAutomaticPeriodSubstitutionEnabled = false;
+  system.defaults.NSGlobalDomain.NSAutomaticQuoteSubstitutionEnabled = false;
+  system.defaults.NSGlobalDomain.NSAutomaticSpellingCorrectionEnabled = false;
+  system.defaults.NSGlobalDomain.NSNavPanelExpandedStateForSaveMode = true;
+  system.defaults.NSGlobalDomain.NSNavPanelExpandedStateForSaveMode2 = true;
+
+  system.defaults.dock.mru-spaces = false;
+  system.defaults.dock.showhidden = true;
+
+  system.defaults.finder.AppleShowAllExtensions = true;
+  system.defaults.finder.QuitMenuItem = true;
+  system.defaults.finder.FXEnableExtensionChangeWarning = false;
+
+  system.defaults.trackpad.Clicking = true;
+
   # List packages installed in system profile. To search by name, run:
   # $ nix-env -qaP | grep wget
   environment.systemPackages = with pkgs; [
