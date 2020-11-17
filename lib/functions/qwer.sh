a6e04cd8406ac24849eafc5149ab8963c3e1bdae
Andrew Seward <awswrd@gmail.com>
2020-11-17 12:59:37 -0800

    Update qwer to also add and update plugins

--- lib/functions/qwer.sh
+++ lib/functions/qwer.sh
@@ -6,19 +6,44 @@ qwer-install() {
 
     local tempdir; tempdir="$(mktemp -d)"
 
+    # ---
+
     echo "Discovering \`.tool-versions\` files ..." && echo
 
-    find "${HOME}/projects" -type f -name '.tool-versions' -print0 \
+    # Temporary: `-print0` didn't work as expected, and I don't really want to
+    #            use -exec
+    # shellcheck disable=SC2038
+    find "${HOME}/projects" -type f -name '.tool-versions' \
       | xargs -t cat "${HOME}/.tool-versions" \
       | sort -u \
       > "${tempdir}/.tool-versions"
 
     local fpath; fpath="${tempdir}/.tool-versions"
 
-    echo && echo "Union of all \`.tool-versions\` files: ${fpath}" && echo
+    # ---
+
+    echo && echo "Union of \`.tool-versions\` files: ${fpath}" && echo
     cat "${fpath}"
     echo
 
+    # ---
+
+    echo "Adding plugins ..." && echo
+    # shellcheck disable=SC2002
+    # Temporary ^^
+    cat "${fpath}" | awk '{ print $1 }' | sort -u \
+      | xargs -t -L1 asdf plugin-add || true
+    echo
+
+    # ---
+
+    echo "Updating plugins ..." && echo
+    asdf plugin-update --all
+    echo
+
+    # ---
+
+    echo "Installing versions ..." && echo
     cd "${tempdir}" && asdf install
   )
 }
