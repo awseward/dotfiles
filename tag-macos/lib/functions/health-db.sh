1e1285bc8721841d568af613586e52573b40ac85
Andrew Seward <awswrd@gmail.com>
2020-12-12 09:51:29 -0800

    Add hdb_config

--- tag-macos/lib/functions/health-db.sh
+++ tag-macos/lib/functions/health-db.sh
@@ -1,6 +1,25 @@
 #!/usr/bin/env bash
 
-hdb_dir="$HOME/.health-db"
+readonly hdb_dir="$HOME/.health-db"
+
+hdb_config() {
+  _generate_default_config() {
+    cat <<DHALL
+      let Config = ${hdb_dir}/Config.dhall
+
+      in Config::{=}
+DHALL
+  }
+
+  local -r config_file="$HOME/.health-db.test.dhall"
+
+  if [ -f "${config_file}" ]; then
+    >&2 echo "File ${config_file} already exists; doing nothing"
+    return
+  fi
+
+  _generate_default_config > "${config_file}"
+}
 
 water() {
   cd "$hdb_dir" || return 1
