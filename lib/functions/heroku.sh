bc9dbb275e8bd05b0bf71386cf66845210ff90b5
Andrew Seward <awswrd@gmail.com>
2020-12-17 23:09:18 -0800

    Add hk_pg_copy_tsv
    This is just a quick and dirty way to spew some pg rows into visidata
    like so:
    
    ```
      hk_pg_copy_tsv <table_name> | vd
    ```
    
    It's fraught with all kinds of issues, like when text columns have tab
    characters, etc. but it's meant to be quick and dirty, so not _really_ a
    deal breaker per se.

--- lib/functions/heroku.sh
+++ lib/functions/heroku.sh
@@ -3,3 +3,17 @@
 hk_cfg_vim() {
   heroku config --shell | parallel echo 'export ' | vim -c 'set syntax=bash' -
 }
+
+hk_pg_copy_tsv() {
+  local -r table_name="$1"
+
+  psql "$(heroku config:get DATABASE_URL)" <<< "
+    COPY ${table_name}
+    TO STDOUT
+    WITH (
+      FORMAT CSV
+    , DELIMITER E'\t'
+    , HEADER ON
+    );
+  "
+}
