c31faad4cc179d97594fec040e2adc63da72c913
Andrew Seward <awswrd@gmail.com>
2021-03-28 18:52:00 -0700

    Put something real basic together
    Put into place via:
    ```sh
    cp ~/.config/backups/backups_rsync_net.template.plist ~/Library/LaunchAgents/seward.andrew.backups_rsync_net.plist
    ```
    
    Launchd snippets:
    ```sh
    launchctl load   -w ~/Library/LaunchAgents/seward.andrew.backups_rsync_net.plist
    launchctl unload -w ~/Library/LaunchAgents/seward.andrew.backups_rsync_net.plist
    ```
    
    May need to run any of the following:
    ```sh
    mkdir -p ~/.local/shared/backups
    touch    ~/.local/shared/backups/rsync-net.log
    ```

---
+++ tag-macos/config/backups/rsync-net.sh
@@ -0,0 +1,60 @@
+#!/usr/bin/env bash
+
+# Based on the following example:
+# https://borgbackup.readthedocs.io/en/stable/quickstart.html#automating-backups
+
+# Setting this, so the repo does not need to be given on the commandline:
+export BORG_REPO='rsync-net:borg-test'
+
+# See the section "Passphrase notes" for more infos.
+export BORG_PASSPHRASE=''
+
+# This needs to be set to use borg 1.X on rsync.net
+export BORG_REMOTE_PATH=/usr/local/bin/borg1/borg1
+
+# some helpers and error handling:
+info() { printf "\n%s %s\n\n" "$( date )" "$*" >&2; }
+trap 'echo $( date ) Backup interrupted >&2; exit 2' INT TERM
+
+info "Starting backup"
+
+# Backup the most important directories into an archive named after
+# the machine this script is currently running on:
+
+borg create              \
+    --verbose            \
+    --list               \
+    --stats              \
+    --show-rc            \
+    --exclude-caches     \
+                         \
+    ::'{hostname}-{now}' \
+                         \
+    ~/borg-test-source   \
+
+backup_exit=$?
+
+info "Pruning repository"
+
+borg prune                 \
+    --list                 \
+    --prefix '{hostname}-' \
+    --show-rc              \
+    --keep-daily    7      \
+    --keep-weekly   4      \
+    --keep-monthly  6      \
+
+prune_exit=$?
+
+# use highest exit code as global exit code
+global_exit=$(( backup_exit > prune_exit ? backup_exit : prune_exit ))
+
+if [ ${global_exit} -eq 0 ]; then
+    info "Backup and Prune finished successfully"
+elif [ ${global_exit} -eq 1 ]; then
+    info "Backup and/or Prune finished with warnings"
+else
+    info "Backup and/or Prune finished with errors"
+fi
+
+exit ${global_exit}
