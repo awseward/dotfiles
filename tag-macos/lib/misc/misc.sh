af5d73630f7aa6f5ce6c47fe1d81a84e79637bee
Andrew Seward <awswrd@gmail.com>
2021-03-29 01:48:51 -0700

    Prune some lines in misc.sh

--- tag-macos/lib/misc/misc.sh
+++ tag-macos/lib/misc/misc.sh
@@ -2,14 +2,8 @@
 
 # NOTE: Try to find a place for things put in here, at least eventually...
 
-# Fuse/Uno
-JAVA_HOME="$(/usr/libexec/java_home)"
-export JAVA_HOME
-
 # Racket
 export RACKET_BIN_DIR='/Applications/Racket v7.0/bin'
-
-__ensure_in_PATH '/Applications/SnowSQL.app/Contents/MacOS'
 
 # shellcheck source=/dev/null
 test -e "$HOME/.iterm2_shell_integration.zsh" && source "$HOME/.iterm2_shell_integration.zsh"
