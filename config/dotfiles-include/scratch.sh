636a5e6d3282ff6d4e11d43f1240516d1807cf91
Andrew Seward <awswrd@gmail.com>
2021-04-27 22:12:35 -0700

    Move scratch files into XDG_CACHE_HOME

--- config/dotfiles-include/scratch.sh
+++ config/dotfiles-include/scratch.sh
@@ -2,7 +2,10 @@
 
 scratch() {
   local -r extension="$1"
-  local -r filepath="$(mktemp -d -t 'mkscratch-XXXX')/scratch.${extension}"
+  local -r base_path="${XDG_CACHE_HOME:-${HOME}/.cache}/scratch"
+
+  mkdir -p "${base_path}"
+  filepath="$(mktemp "${base_path}/XXXXXXXX.scratch.${extension}")"
 
   "${EDITOR}" "${filepath}"
 }
