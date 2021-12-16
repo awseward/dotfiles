b0d3508d6460ef8a45db18304523e455d37b436c
Andrew Seward <awswrd@gmail.com>
2021-12-15 20:38:52 -0800

    Add remotes to standard props

--- git-hooks/script.sh
+++ git-hooks/script.sh
@@ -9,10 +9,29 @@ fmt_hook_args() {
 
 fmt_standard_props() {
   local -r hook_args_json_arr="$("$0" fmt_hook_args "$@")"
+  local remotes_json_obj; remotes_json_obj="$(git remote -v | "$0" parse_git_remote_v)"; readonly remotes_json_obj
   jq \
     --compact-output \
     --argjson hook_args "$hook_args_json_arr" \
-    '. + { hook_args: $hook_args } ' <<< '{}'
+    --argjson remotes "$remotes_json_obj" \
+    '. + { hook_args: $hook_args } + $remotes' <<< '{}'
+}
+
+parse_git_remote_v() {
+  sed -e 's/ (\(.*\))$/ \1/' \
+  | while read -r -a parts; do
+      jq \
+        --compact-output \
+        --arg 'name' "${parts[0]}" \
+        --arg 'url'  "${parts[1]}" \
+        --arg 'type' "${parts[2]}" \
+        '. + {
+          name: $name,
+          url: $url,
+          type: $type
+        } ' <<< '{}'
+    done \
+  | jq -cs '{ remotes: . }'
 }
 
 "$@"
