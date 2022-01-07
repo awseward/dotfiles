6258ab3ad30ca3f3f77fce49fd9fbf57aaa515c2
Andrew Seward <awswrd@gmail.com>
2022-01-07 15:37:06 -0800

    Fix trailing `\n` issue with `pbg{b,s}`

--- config/dotfiles-include/aliases.sh
+++ config/dotfiles-include/aliases.sh
@@ -22,8 +22,8 @@ alias gm='git checkout $(git symbolic-ref refs/remotes/origin/HEAD | sed "s@^ref
 # Two shortcomings of this at the moment:
 # - It's super MacOS-specific
 # - The concept of "tee to clipboard" is probably a little more generalizable
-alias pbgb='git rev-parse --abbrev-ref HEAD | tee >(pbcopy)'
-alias pbgs='git rev-parse HEAD | tee >(pbcopy)'
+alias pbgb='git rev-parse --abbrev-ref HEAD | tee >(xargs echo -n | pbcopy)'
+alias pbgs='git rev-parse HEAD | tee >(xargs echo -n | pbcopy)'
 
 ### hub
 alias hc='hub compare'
