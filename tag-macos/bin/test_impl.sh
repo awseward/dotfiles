85e4dd24ce7dc8e6c11187d74871ef6272e66ecb
Andrew Seward <awswrd@gmail.com>
2022-06-14 14:58:45 -0700

    Some tweaks

--- tag-macos/bin/test_impl.sh
+++ tag-macos/bin/test_impl.sh
@@ -15,19 +15,21 @@ render_event() { git ghl --color=always -n1 && echo && jq --color-output; }
 announce() {
   local -r event="$(cat -)"
   local -r status="$(jq -r '.status' <<< "${event}")"
+  # Chooses a random English-speaking voice.
   local -r voice="$(say -v '?' | grep '# Hello' | awk '{ print $1 }' | shuf | head -n1)"
 
   (
     jq '"C I status: \(.status)."' <<< "${event}"
     _status_assessment "${status}"
-    # git log --pretty=format:'%s' | head -n1
   ) | say -v "${voice}"
 
   say -r 275 -v "${voice}" "
-      This has been ${voice}, with your CI announcement for \"$(jq -r '.name' <<< "${event}")\".
+      This has been ${voice}, with your C I announcement for \"$(jq -r '.name' <<< "${event}")\".
       That status again, was ${status}.
-      Have a nice day!
     "
+  sleep '0.15'
+
+  say -v "${voice}" 'Have a nice day!'
 }
 
 _status_assessment() {
