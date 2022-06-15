8fa60ede78db583838f8357fc16406d0c4a4170f
Andrew Seward <awswrd@gmail.com>
2022-06-14 22:57:46 -0700

    Try and get some other sounds in there

--- tag-macos/bin/test_impl.sh
+++ tag-macos/bin/test_impl.sh
@@ -10,6 +10,44 @@ fetch_event() { hub ci-status-v by_name 'ci/circleci: build'; }
 
 render_event() { git ghl --color=always -n1 && echo && jq --color-output; }
 
+_sound() {
+  local -r status="$1"
+
+  case "${status}" in
+    success)
+      play -n synth 5 \
+        sin %-31 sin %-19  sin %+0  sq %+4  sq %+7  sq %+9  sq %+14  sq %+16 \
+        delay   0        0         0.10     0.20    0.30    0.40    0.50     0.60    \
+        overdrive \
+        reverb \
+        lowpass 2600 \
+        fade 0 0 5 \
+        2>/dev/null \
+        &
+      ;;
+    failure|error|action_required|cancelled|timed_out)
+      play -n synth 0.06 \
+        sin %-12 sq %+0 sq %-6 sq %+4 sq %+1 brownnoise %-24 \
+        pad 0.03@0.06 \
+        repeat 2 \
+        overdrive 100 vol 0.8 \
+        lowpass 4000 \
+        2>/dev/null \
+        &
+      ;;
+    *)
+      # IDK this sound is sorta dumb
+      play -n synth 1 \
+        tri %-3-%-10 \
+        overdrive 60 \
+        reverb \
+        fade 0 0 0.5 \
+        2>/dev/null \
+        &
+      ;;
+  esac
+}
+
 announce() {
   local -r event="$(cat -)"
   local -r status="$(jq -r '.status' <<< "${event}")"
@@ -18,15 +56,7 @@ announce() {
 
   # This is a pleasant sounding chord, but we should probably have something a
   # little spicier for error status
-  type -f play >/dev/null 2>/dev/null && play -n \
-    synth 5 sin %-31 sin %-19  sin %+0  sq %+4  sq %+7  sq %+9  sq %+14  sq %+16 \
-    delay   0        0         0.10     0.20    0.30    0.40    0.50     0.60    \
-    overdrive \
-    reverb \
-    lowpass 2600 \
-    fade 0 0 5 \
-    2>/dev/null \
-    &
+  type -f play >/dev/null 2>/dev/null && _sound "${status}"
 
   sleep 1
 
