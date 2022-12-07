f1bb9a2c443037c2dbce4e511b0a46f7d126bc4e
Andrew Seward <awswrd@gmail.com>
2022-12-06 21:18:16 -0800

    More tweaks
    Not really doing a great job at "atomic" commits or whatever, but who
    really cares, tbh?

--- hooks/post-up/zzz-original.sh
+++ hooks/post-up/zzz-original.sh
@@ -17,18 +17,19 @@ _install_vim_plugins() {
       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
   fi
 
-  # NOTE: If we don't pipe this dummy file in, along with the `-` at the end,
-  #       vim displays a warning about input not being from a terminal, and
-  #       then breaks the terminal once it exits.
-  echo 'foo' | vim -u "$HOME/.vim/vimrc.plugins" +PlugInstall '+qa!' -
+  nvim -u "$HOME/.vim/vimrc.plugins" +PlugInstall '+qa!'
 }
 
 _trigger_custom_post_ups() {
   test -e "$HOME/.env-specific/post-up" && "$HOME/.env-specific/post-up"
 }
 
+_default() {
+  _symlink_projects_dotfiles_dir
+  _install_vim_plugins
+  _trigger_custom_post_ups
+}
+
 # ---
 
-_symlink_projects_dotfiles_dir
-_install_vim_plugins
-_trigger_custom_post_ups
+"${@:-_default}"
