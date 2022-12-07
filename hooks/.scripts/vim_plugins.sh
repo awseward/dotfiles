e2099e7f3e649de2dec936bdad9d59d204ec14f7
Andrew Seward <awswrd@gmail.com>
2022-12-07 13:57:02 -0800

    Symink `~/.config/nvim/init.vim -> ~/.vimrc`
    I think eventually I'll just fully commit to nvim so this symlink won't
    be necessary at all anymore, but this'll be fine in the meantime.

--- hooks/.scripts/vim_plugins.sh
+++ hooks/.scripts/vim_plugins.sh
@@ -8,6 +8,13 @@ set -euo pipefail
 pre-up() { _not_implemented pre-up "$@"; }
 
 post-up() {
+  # NOTE: This one line is just a little bit out of place in this script as-is.
+  # It would be good to either move it out to its own script or rename this one
+  # to be a little less specific.
+  ln -s "$HOME/.vimrc" "$HOME/.config/nvim/init.vim" || true
+
+  # ---
+
   local -r plug_dir="$HOME/.vim/autoload/plug.vim"
 
   # Install plug.vim and plugins
