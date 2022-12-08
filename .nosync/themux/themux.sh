9bf3fda524c64eadfd2921b127d2f4158af5d981
Andrew Seward <awswrd@gmail.com>
2022-12-08 12:22:29 -0800

    More tweaks
    I guess I wasn't quite done. Changes:
    
    - Make `themux.sh` less brittle re: working direcotry; i.e. you can call
      it from anywhere now and it should just work.
    
    - Add a `^l` binding as a sort of rough "preview".

--- .nosync/themux/themux.sh
+++ .nosync/themux/themux.sh
@@ -2,49 +2,53 @@
 
 set -euo pipefail
 
-# shellcheck disable=SC2120
-_render_theme() {
-  local -r theme_name="${1:-"$(_read_configured_theme_name)"}"
-  local -r theme_defn_filepath='./lib/Theme.dhall'
-  local -r themes_filepath="./config/themes.dhall"
+_script_dir_abspath() { realpath "$(dirname "$0")"; }
+_in_script_dir() { echo "$(_script_dir_abspath)/$1"; }
 
-  dhall text <<< "($theme_defn_filepath).show ($themes_filepath).$theme_name"
-}
+_themes_filepath() { _in_script_dir 'config/themes.dhall'; }
 
-_get_theme_name_config_filepath() {
+_theme_name_config_filepath() {
   # TODO: Check XDG_CONFIG_HOME here, too.
   echo "$HOME/.config/themux/theme_name"
 }
 
 _write_tmux_colors_config() { cat - > "$HOME/.config/tmux/tmux.conf.colors"; }
-_write_theme_name_config()  { cat - > "$(_get_theme_name_config_filepath)";  }
+_write_theme_name_config()  { cat - > "$(_theme_name_config_filepath)";  }
 
-_read_configured_theme_name() {
-  # TODO: Check XDG_CONFIG_HOME here, too.
-  cat "$(_get_theme_name_config_filepath)"
-}
+_read_theme_name_config() { cat "$(_theme_name_config_filepath)"; }
 
 _reload_tmux() { tmux source-file "${HOME}/.tmux.conf"; }
 
-list() {
-  local -r themes_filepath="./config/themes.dhall"
+# shellcheck disable=SC2120
+_render_theme() {
+  local -r theme_name="${1:-"$(_read_theme_name_config)"}"
+  local -r module_filepath="$(_in_script_dir 'lib/Theme.dhall')"
 
-  dhall-to-json <<< "$themes_filepath" | jq -c -r 'keys[]'
+  dhall text <<< "($module_filepath).show ($(_themes_filepath)).$theme_name"
 }
 
+apply() {
+  local -r name="$1"
+
+  _write_theme_name_config <<< "$name"
+  _render_theme "$name" | _write_tmux_colors_config
+  _reload_tmux
+}
+
+list() { dhall-to-json <<< "$(_themes_filepath)" | jq -c -r 'keys[]'; }
+
 choose() {
-  local -r current_theme="$(_read_configured_theme_name || echo 'none')"
+  local -r current_theme="$(_read_theme_name_config)"
   local choice; choice="$(
     list | fzf \
-      --header "Current: $current_theme" \
-      --height '50%'                     \
-      --layout 'reverse'                 \
+      --bind   'ctrl-l:execute:('"$0"' apply {})' \
+      --header "Originally using: ${current_theme:-none}" \
+      --height '50%' \
+      --layout 'reverse' \
       --prompt 'Choose a theme: '
   )"; readonly choice
 
-  _write_theme_name_config <<< "$choice"
-  _render_theme "$choice" | _write_tmux_colors_config
-  _reload_tmux
+  apply "$choice"
 }
 
 help() {
