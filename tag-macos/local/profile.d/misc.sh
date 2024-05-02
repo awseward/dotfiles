5c9825f14db6e69c4e60da80047356b1a4dc1d35
Andrew Seward <awswrd@gmail.com>
2024-05-01 19:59:38 -0700

    Remove some prior iTerm2 supporting jank
    Good riddance to the plist file in particular.

--- tag-macos/local/profile.d/misc.sh
+++ tag-macos/local/profile.d/misc.sh
@@ -2,9 +2,6 @@
 
 # NOTE: Try to find a place for things put in here, at least eventually...
 
-# shellcheck source=/dev/null
-test -e "$HOME/.iterm2_shell_integration.zsh" && source "$HOME/.iterm2_shell_integration.zsh"
-
 if [ -z "$HOMEBREW_GITHUB_API_TOKEN" ]; then
   HOMEBREW_GITHUB_API_TOKEN="$(keychain_get_env_var 'HOMEBREW_GITHUB_API_TOKEN')"
   export HOMEBREW_GITHUB_API_TOKEN
