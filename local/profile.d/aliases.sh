82e1fe8bc7b5f3bc0148e3f09276db2d34eb9b61
Andrew Seward <awswrd@gmail.com>
2024-05-29 14:36:57 -0700

    Update pbg{b,s} just a little bit
    I find it useful for them to print some context out, too

--- local/profile.d/aliases.sh
+++ local/profile.d/aliases.sh
@@ -19,11 +19,11 @@ alias vim='nvim'
 alias g='git'
 # TODO: Find a better way to detect the "main" branch
 alias gm='git checkout $(git symbolic-ref refs/remotes/origin/HEAD | sed "s@^refs/remotes/origin/@@") && git pull origin $(git symbolic-ref refs/remotes/origin/HEAD | sed "s@^refs/remotes/origin/@@")'
-# Two shortcomings of this at the moment:
-# - It's super MacOS-specific
-# - The concept of "tee to clipboard" is probably a little more generalizable
-alias pbgb='git rev-parse --abbrev-ref HEAD | tee >(xargs echo -n | pbcopy)'
-alias pbgs='git rev-parse HEAD | tee >(xargs echo -n | pbcopy)'
+# # Two shortcomings of this at the moment:
+# # - It's super MacOS-specific
+# # - The concept of "tee to clipboard" is probably a little more generalizable
+# alias pbgb='git rev-parse --abbrev-ref HEAD | tee >(xargs echo -n | pbcopy)'
+# alias pbgs='git rev-parse HEAD | tee >(xargs echo -n | pbcopy)'
 
 ### hub
 alias hc='hub compare'
--- local/profile.d/git.sh
+++ local/profile.d/git.sh
@@ -4,3 +4,11 @@ git_current_branch() { git rev-parse --abbrev-ref HEAD; }
 
 # This is mostly just here for backwards compatibility
 git_delete_pruneables() { git delete-pruneable-branches "$@"; }
+
+# (P)(B)copy (G)it (B)ranch name
+pbgb_q() { git rev-parse --abbrev-ref HEAD | tee >(xargs echo -n | pbcopy); }
+# (P)(B)copy (G)it commit (S)ha
+pbgs_q() { git rev-parse HEAD | tee >(xargs echo -n | pbcopy); }
+
+pbgb() { >&2 git msg; >&2 echo; pbgb_q; }
+pbgs() { >&2 git msg; >&2 echo; pbgs_q; }
