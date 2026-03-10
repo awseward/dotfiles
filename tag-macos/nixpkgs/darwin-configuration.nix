50f1471bf9793869d74630c77673c9bf5ed266b9
Andrew Seward <awswrd@gmail.com>
2026-03-09 17:12:48 -0700

    Group some stuff under `system.defaults`

--- tag-macos/nixpkgs/darwin-configuration.nix
+++ tag-macos/nixpkgs/darwin-configuration.nix
@@ -3,46 +3,52 @@
 let
   unstable = import <nixos-unstable> { config = { allowUnfree = true; }; };
 in {
-  system.defaults.NSGlobalDomain.AppleEnableMouseSwipeNavigateWithScrolls = false;
-  system.defaults.NSGlobalDomain.AppleEnableSwipeNavigateWithScrolls = false;
-  system.defaults.NSGlobalDomain.AppleInterfaceStyleSwitchesAutomatically = true;
-  system.defaults.NSGlobalDomain.AppleKeyboardUIMode = 3;
-  system.defaults.NSGlobalDomain.ApplePressAndHoldEnabled = true;
-  system.defaults.NSGlobalDomain.AppleShowScrollBars = "Always";
-  system.defaults.NSGlobalDomain.InitialKeyRepeat = 11;
-  system.defaults.NSGlobalDomain.KeyRepeat = 2;
-  system.defaults.NSGlobalDomain.NSAutomaticCapitalizationEnabled = false;
-  system.defaults.NSGlobalDomain.NSAutomaticDashSubstitutionEnabled = false;
-  system.defaults.NSGlobalDomain.NSAutomaticPeriodSubstitutionEnabled = false;
-  system.defaults.NSGlobalDomain.NSAutomaticQuoteSubstitutionEnabled = false;
-  system.defaults.NSGlobalDomain.NSAutomaticSpellingCorrectionEnabled = false;
-  system.defaults.NSGlobalDomain.NSNavPanelExpandedStateForSaveMode = true;
-  system.defaults.NSGlobalDomain.NSNavPanelExpandedStateForSaveMode2 = true;
-  system.defaults.NSGlobalDomain.NSTableViewDefaultSizeMode = 1;
-  system.defaults.NSGlobalDomain.NSUseAnimatedFocusRing = false;
-  system.defaults.NSGlobalDomain.NSWindowResizeTime = 0.01;
-  system.defaults.NSGlobalDomain.PMPrintingExpandedStateForPrint = true;
-  system.defaults.NSGlobalDomain.PMPrintingExpandedStateForPrint2 = true;
+  system.defaults.NSGlobalDomain = {
+    AppleEnableMouseSwipeNavigateWithScrolls = false;
+    AppleEnableSwipeNavigateWithScrolls = false;
+    AppleInterfaceStyleSwitchesAutomatically = true;
+    AppleKeyboardUIMode = 3;
+    ApplePressAndHoldEnabled = true;
+    AppleShowScrollBars = "Always";
+    InitialKeyRepeat = 11;
+    KeyRepeat = 2;
+    NSAutomaticCapitalizationEnabled = false;
+    NSAutomaticDashSubstitutionEnabled = false;
+    NSAutomaticPeriodSubstitutionEnabled = false;
+    NSAutomaticQuoteSubstitutionEnabled = false;
+    NSAutomaticSpellingCorrectionEnabled = false;
+    NSNavPanelExpandedStateForSaveMode = true;
+    NSNavPanelExpandedStateForSaveMode2 = true;
+    NSTableViewDefaultSizeMode = 1;
+    NSUseAnimatedFocusRing = false;
+    NSWindowResizeTime = 0.01;
+    PMPrintingExpandedStateForPrint = true;
+    PMPrintingExpandedStateForPrint2 = true;
+  };
 
   system.defaults.alf.stealthenabled = 1;
 
-  system.defaults.dock.autohide = false;
-  system.defaults.dock.enable-spring-load-actions-on-all-items = true;
-  system.defaults.dock.launchanim = false;
-  system.defaults.dock.mouse-over-hilite-stack = true;
-  system.defaults.dock.mru-spaces = false;
-  system.defaults.dock.show-recents = false;
-  system.defaults.dock.showhidden = true;
-  system.defaults.dock.tilesize = 8;
+  system.defaults.dock = {
+    autohide = false;
+    enable-spring-load-actions-on-all-items = true;
+    launchanim = false;
+    mouse-over-hilite-stack = true;
+    mru-spaces = false;
+    show-recents = false;
+    showhidden = true;
+    tilesize = 8;
+  };
 
-  system.defaults.finder._FXShowPosixPathInTitle = true;
-  system.defaults.finder.AppleShowAllExtensions = true;
-  system.defaults.finder.AppleShowAllFiles = true;
-  system.defaults.finder.FXDefaultSearchScope = "SCcf"; # Current folder
-  system.defaults.finder.FXEnableExtensionChangeWarning = false;
-  system.defaults.finder.FXPreferredViewStyle = "Nlsv"; # List view
-  system.defaults.finder.QuitMenuItem = true;
-  system.defaults.finder.ShowPathbar = true;
+  system.defaults.finder = {
+    _FXShowPosixPathInTitle = true;
+    AppleShowAllExtensions = true;
+    AppleShowAllFiles = true;
+    FXDefaultSearchScope = "SCcf"; # Current folder
+    FXEnableExtensionChangeWarning = false;
+    FXPreferredViewStyle = "Nlsv"; # List view
+    QuitMenuItem = true;
+    ShowPathbar = true;
+  };
 
   system.defaults.trackpad.Clicking = true;
 
